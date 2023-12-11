3ac43d5b8426d8048e6c43a3ab2e4a3104a15a9c
JordanSanders <83708649+YuVasIv@users.noreply.github.com>
2023-12-11 13:29:21 +0200

    Update Person.Password.sql

--- Tables/Person.Password.sql
+++ Tables/Person.Password.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [Person].[Password] (
   [BusinessEntityID] [int] NOT NULL,
   [PasswordHash] [varchar](128) NOT NULL,
-  [PasswordSalt] [varchar](10) NOT NULL,
+  [PasswordSalt] [varchar](20) NOT NULL,
   [rowguid] [uniqueidentifier] NOT NULL CONSTRAINT [DF_Password_rowguid] DEFAULT (newid()) ROWGUIDCOL,
   [ModifiedDate] [datetime] NOT NULL CONSTRAINT [DF_Password_ModifiedDate] DEFAULT (getdate()),
   CONSTRAINT [PK_Password_BusinessEntityID] PRIMARY KEY CLUSTERED ([BusinessEntityID])
