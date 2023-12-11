14c912b9549c5b4fb75a4fe33a464cd52f403218
YULIA VASILENKO <yuliav@devart.com>
2023-12-11 15:39:37 +0200

    Closes #1

---
+++ Tables/HumanResources.Customers.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [HumanResources].[Customers] (
+  [PersonID] [int] NULL,
+  [LastName] [varchar](255) NULL,
+  [FirstName] [varchar](255) NULL,
+  [Address] [varchar](255) NULL,
+  [City] [varchar](255) NULL
+)
+ON [PRIMARY]
+GO
